553855af71cf34952e8cb6236535e2cc0012a197
abdelrahman <abdelrahmanreda00001@gmail.com>
2024-03-06 01:43:45 +0200

    Create Book And Category Models And Migrate Them

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from . models import Category , Book
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title' , 'author' , 'price' , 'category' , 'status' , 'rental_price_day' , 'active']
--- books/models.py
+++ books/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
 # Create your models here.
+
+def image_book_upload(instance,file_name:str):
+    extension = file_name.split('.')[1]
+    return f'books\{instance.name}.{extension}'
+
+
+def image_author_upload(instance,file_name:str):
+    extension = file_name.split('.')[1]
+    return f'author\{instance.name}.{extension}'
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=150)
+
+    
+
+    class Meta:
+        verbose_name = ("Category")
+        verbose_name_plural = ("Categories")
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
+
+class Book(models.Model):
+
+
+    status_choices = [
+        ('available', 'available'),
+        ('rental', 'rental'),
+        ('sold', 'sold'),
+    ]
+
+
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=150)
+    photo_book = models.ImageField(upload_to=image_book_upload,null=True,blank=True)
+    photo_author = models.ImageField(upload_to=image_author_upload,null=True,blank=True)
+    pages = models.IntegerField(null=True,blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2,null=True,blank=True)
+    rental_price_day = models.DecimalField(max_digits=5, decimal_places=2,null=True,blank=True)
+    rental_period = models.IntegerField(null=True,blank=True)
+    active = models.BooleanField(default = True)
+    status = models.CharField(max_length=50 , choices = status_choices , null = True , blank = True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,null=True,blank=True)
+
+    
+
+    class Meta:
+        verbose_name = ("Book")
+        verbose_name_plural = ("Books")
+
+    def __str__(self):
+        return self.title
