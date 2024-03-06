83223903f27a4acf48f62833b85ec29c0cacca28
abdelrahman <abdelrahmanreda00001@gmail.com>
2024-03-06 15:30:35 +0200

    Fetch Books And Categories And Put Them In The Context

--- project/views.py
+++ project/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
-
+from books.models import Category , Book
 def home(request):
-    return render(request, 'home/home.html')
+    books = Book.objects.all().filter(active = True)
+    categories = Category.objects.all().filter(active = True)
+    context = {
+        'books' : books,
+        'categories' : categories,
+    }
+    return render(request , 'home/home.html' , context)
