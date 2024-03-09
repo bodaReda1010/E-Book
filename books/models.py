a3840365c9d184d059637c9d1eae8b12ade4b667
abdelrahman <abdelrahmanreda00001@gmail.com>
2024-03-09 21:38:49 +0200

    Fiter The Books By Category

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
 
@@ -22,6 +23,9 @@ class Category(models.Model):
         verbose_name = ("Category")
         verbose_name_plural = ("Categories")
 
+    def get_url(self):
+        return reverse('home:category_slug',args=[self.slug,])
+    
     def __str__(self):
         return self.name
     
--- books/views.py
+++ books/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Category , Book
+def home(request , price_sold = 0 , price_rental = 0 , category_slug = None):
+    if category_slug != None:
+        category = Category.objects.get(slug=category_slug)
+        books = Book.objects.filter(category = category , active = True)
+    else:
+        books = Book.objects.all().filter(active = True)
 
-# Create your views here.
+        
+    categories = Category.objects.all()
+    sold_books = Book.objects.filter(status = 'sold')
+    rental_books = Book.objects.filter(status = 'rental')
+    for book in sold_books:
+        price_sold += book.price
+
+    for r in rental_books:
+        price_rental += r.total_rental
+
+    total = price_sold + price_rental
+
+
+
+    context = {
+        'books' : books,
+        'categories' : categories,
+        'total':total,
+
+    }
+    return render(request , 'home/home.html' , context)
